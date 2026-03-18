384cb9828965c838cb829112e03a7f451d6fcf82
Erik <erithor@stud.ntnu.no>
2026-03-18 21:00:22 +0100

    ADDED: -Added doc -Added functionality to work with program flow

--- internal/handlers/status_handler.go
+++ internal/handlers/status_handler.go
@@ -8,17 +8,23 @@ import (
 	"time"
 )
 
-/*
-Routes to the handler for the status endpoint
-*/
-func StatusRouter(router *http.ServeMux) {
-	service := services.StatusService(time.Now())
-	router.HandleFunc(config.STATUS_PAGE_PATH, statusHandler(service))
+// Hold the status service to be used by the handler
+var statusService *services.StatusInternal
+
+// executes once, on runtime
+func init() {
+	statusService = services.StatusService(time.Now())
 }
 
-/*
-Handles GET request to the /countryinfo/v1/status endpoint
-*/
+// StatusRouter
+// Routes to the statusHandler with the application-wide status service
+func StatusRouter(router *http.ServeMux) {
+	router.HandleFunc(config.STATUS_PAGE_PATH, statusHandler(statusService))
+}
+
+// statusHandler
+// handles GET requests for the status endpoint
+// gives error on all other methods
 func statusHandler(service *services.StatusInternal) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
@@ -26,75 +32,9 @@ func statusHandler(service *services.StatusInternal) http.HandlerFunc {
 			return
 		}
 
-		healthStatuses := probeAllEndpoints()
-
-		status := service.GetStatus(healthStatuses)
+		//go to service layer to get status
+		status := service.GetStatus()
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(status)
 	}
-}
-
-// sends a head request to the endpoint to get the status
-func probeHeadEndPoint(url string) (int, error) {
-	resp, err := http.Head(url)
-	if err != nil {
-		return http.StatusServiceUnavailable, err
-	}
-	defer resp.Body.Close()
-
-	return resp.StatusCode, nil
-}
-
-// maybe return error, dont think its needed as if error set statuscode 503
-// todo: check if switch to HEAD method works
-func getStatus(url string, headerKey string, headerValue string) int {
-	client := http.Client{}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return http.StatusServiceUnavailable
-	}
-
-	// set api key header if provided
-	if headerValue != "" {
-		req.Header.Set("X-API-Key", headerValue)
-	}
-
-	// set custom header if provided
-	if headerKey != "" {
-		req.Header.Set(headerKey, headerValue)
-	}
-
-	// runs GET request on URL
-	res, err := client.Do(req)
-	if err != nil {
-		return http.StatusServiceUnavailable
-	}
-	defer res.Body.Close()
-
-	return res.StatusCode
-}
-
-// returns a map of the status codes for all endpoints
-func probeAllEndpoints() map[string]int {
-	endpoints := map[string]string{
-		//cannot have the other methods as they need custom headers or Head not implemented
-		"countries": config.REST_COUNTRIES_API_PROBE,
-		"metro":     config.METRO_API,
-	}
-
-	healthStatuses := make(map[string]int)
-	for name, url := range endpoints {
-		statusCode, err := probeHeadEndPoint(url)
-		if err != nil {
-			statusCode = http.StatusServiceUnavailable
-		}
-		healthStatuses[name] = statusCode
-	}
-
-	healthStatuses["openaq"] = getStatus(config.OPENAQ_PROBE, "", config.OPENAQ_KEY)
-	healthStatuses["nominatim"] = getStatus(config.NOMINATIM_PROBE, "User-Agent", "evdash/")
-	healthStatuses["currency"] = getStatus(config.CURRENCIES_API_PROBE, "", "")
-
-	return healthStatuses
 }
