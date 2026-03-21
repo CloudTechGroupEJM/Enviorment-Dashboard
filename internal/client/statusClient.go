9c63f277977f9401e7d437919984025e1e8475d4
Erik <erithor@stud.ntnu.no>
2026-03-21 21:13:01 +0100

    ADDED: statusClient -handles calls to external APIs -Get the status codes

---
+++ internal/client/statusClient.go
@@ -0,0 +1,57 @@
+package client
+
+import (
+	"net/http"
+	"time"
+)
+
+// provides HTTP functionaltiy
+type StatusClient struct {
+	httpClient *http.Client
+}
+
+// NewStatusClient
+// Creates a status client to execute HTTP calls.
+func NewStatusClient() *StatusClient {
+	return &StatusClient{
+		httpClient: &http.Client{Timeout: 5 * time.Second},
+	}
+}
+
+// probeHeadEndPoint
+// sends a head request to the endpoint to get the status code
+// only useful for endpoints that support it
+func (sc *StatusClient) ProbeHeadEndpoint(url string) (int, error) {
+	resp, err := sc.httpClient.Head(url)
+	if err != nil {
+		return http.StatusServiceUnavailable, err
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode, nil
+}
+
+// ProbeGetEndpoint
+// gets the status code, with configurable header key and header value
+// some endpoint need this to allow for usage
+func (sc *StatusClient) ProbeGetEndpoint(url string, headerKey string, headerValue string) int {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return http.StatusServiceUnavailable
+	}
+
+	if headerValue != "" {
+		req.Header.Set("X-API-Key", headerValue)
+	}
+
+	if headerKey != "" {
+		req.Header.Set(headerKey, headerValue)
+	}
+
+	res, err := sc.httpClient.Do(req)
+	if err != nil {
+		return http.StatusServiceUnavailable
+	}
+	defer res.Body.Close()
+
+	return res.StatusCode
+}
