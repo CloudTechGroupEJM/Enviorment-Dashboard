93981c5f799f0702b7bcd4adea0f323ce5c0473e
Erik <erithor@stud.ntnu.no>
2026-03-30 16:50:54 +0200

    ADDED: metroStruct.go -added structs for the metro API

---
+++ internal/structs/metroStruct.go
@@ -0,0 +1,13 @@
+package structs
+
+type MetroAPIIncoming struct {
+	Daily struct {
+		Temperature2mMean []float64 `json:"temperature_2m_mean"`
+		PrecipitationSum  []float64 `json:"precipitation_sum"`
+	} `json:"daily"`
+}
+
+type MetroResponse struct {
+	MeanTemperature   string
+	MeanPrecipitation string
+}
