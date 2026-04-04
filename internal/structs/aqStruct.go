4c8c7db1ca82c722e73333211ec7c23a965fb8ac
Erik <erithor@stud.ntnu.no>
2026-04-04 22:32:04 +0200

    ADDED:aqStruct.go -Added structs for the open AQ endpoint

---
+++ internal/structs/aqStruct.go
@@ -0,0 +1,30 @@
+package structs
+
+// v3/locations response
+type AirQualityIncoming struct {
+	Results []struct {
+		ID      int `json:"id"`
+		Sensors []struct {
+			ID        int `json:"id"`
+			Parameter struct {
+				Name string `json:"name"` // "pm25", "pm10"
+			} `json:"parameter"`
+		} `json:"sensors"`
+	} `json:"results"`
+}
+
+// v3/locations/{id}/latest response
+type LatestIncoming struct {
+	Results []struct {
+		Value     float64 `json:"value"`
+		SensorsID int     `json:"sensorsId"`
+		Parameter string  `json:"parameter"`
+	} `json:"results"`
+}
+
+// Represents the outgoing air quality data in the dashboard response
+type AqResponse struct {
+	PM25  float64 `json:"pm25"`
+	PM10  float64 `json:"pm10"`
+	Level string  `json:"level"`
+}
